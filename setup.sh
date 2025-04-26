fdf761dd74fe7e70511a6f5736bd787b2ce2aeab
lamtota40 <lamtota40@gmail.com>
2025-04-26 18:41:07 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 sudo apt install apache2 -y
+sudo apt install php php-curl libapache2-mod-php -y
+sudo apt install mysql-server -y
+sudo apt install phpmyadmin -y
+
 sudo systemctl status apache2
-sudo apt install php php-curl libapache2-mod-php
 php -v
-sudo apt install mysql-server
-sudo apt install phpmyadmin

07f231223c6afed30ddbefb3ec4e526ad5367c72
lamtota40 <lamtota40@gmail.com>
2025-04-26 18:51:35 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,12 @@ sudo apt install php php-curl libapache2-mod-php -y
 sudo apt install mysql-server -y
 sudo apt install phpmyadmin -y
 
+sudo mysql
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'passadmin';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+exit
+
+
 sudo systemctl status apache2
 php -v

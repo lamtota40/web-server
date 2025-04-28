c765e9cb85fbc8e7edc6ccfe5b82ded6fd9953f4
lamtota40 <lamtota40@gmail.com>
2025-04-28 13:47:22 +0700

    Update setup_web-server.sh

--- setup_web-server.sh
+++ setup_web-server.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+# Pastikan script dijalankan sebagai root
 if [ "$(id -u)" -ne 0 ]; then
     echo "Harus dijalankan sebagai root!"
     exit 1
 fi
+
 sudo apt update -y
 clear
+
 echo "======================================"
 echo "        Menu Instalasi Server         "
 echo "======================================"
 echo ""
 echo "1. Install Web Server"
 echo "2. Install FTP/FTPS"
-echo "3. Certificate Web (HTTPS)"
+echo "3. Install Certificate Web (HTTPS)"
 echo "4. Uninstall ALL"
 echo ""
 echo "======================================"
@@ -34,21 +37,21 @@ case $pilihan in
                 sudo apt install apache2 php libapache2-mod-php -y
                 sudo systemctl enable apache2
                 sudo systemctl restart apache2
-                
+
                 sudo apt install php-curl php-mysql certbot python python3 -y
-sudo apt install mysql-server phpmyadmin -y
+                sudo apt install mysql-server phpmyadmin -y
 
-sudo chown -R www-data:www-data /var/www/html
-sudo chmod -R 755 /var/www/html
+                sudo chown -R www-data:www-data /var/www/html
+                sudo chmod -R 755 /var/www/html
 
-sudo mysql <<EOF
+                sudo mysql <<EOF
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'Abcd1234!';
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EXIT
 EOF
-sudo systemctl enable mysql
-sudo systemctl restart mysql
+                sudo systemctl enable mysql
+                sudo systemctl restart mysql
                 ;;
             2)
                 echo "Anda memilih Nginx (LEMP)"
@@ -60,19 +63,19 @@ sudo systemctl restart mysql
                 sudo rm -f /var/www/html/index.nginx-debian.html
 
                 sudo apt install php-curl php-mysql certbot python python3 -y
-sudo apt install mysql-server phpmyadmin -y
+                sudo apt install mysql-server phpmyadmin -y
 
-sudo chown -R www-data:www-data /var/www/html
-sudo chmod -R 755 /var/www/html
+                sudo chown -R www-data:www-data /var/www/html
+                sudo chmod -R 755 /var/www/html
 
-sudo mysql <<EOF
+                sudo mysql <<EOF
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'Abcd1234!';
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EXIT
 EOF
-sudo systemctl enable mysql
-sudo systemctl restart mysql
+                sudo systemctl enable mysql
+                sudo systemctl restart mysql
                 ;;
             *)
                 echo "Pilihan tidak tersedia!"
@@ -80,18 +83,20 @@ sudo systemctl restart mysql
         esac
         ;;
     2)
+        clear
         echo "Pilihan anda: Install FTP/FTPS"
         sudo apt install vsftpd openssl -y
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/ssl/private/vsftpd-selfsigned.key \
-    -out /etc/ssl/certs/vsftpd-selfsigned.crt \
-    -subj "/C=US/ST=State/L=City/O=Organization/OU=IT Department/CN=localhost"
-sudo useradd -m -d /var/www/html -s /bin/bash admin
-echo "admin:Abcd1234!" | sudo chpasswd
-sudo chown -R admin:admin /var/www/html
-sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.bak
+        sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+            -keyout /etc/ssl/private/vsftpd-selfsigned.key \
+            -out /etc/ssl/certs/vsftpd-selfsigned.crt \
+            -subj "/C=US/ST=State/L=City/O=Organization/OU=IT Department/CN=localhost"
 
-sudo bash -c 'cat > /etc/vsftpd.conf' <<EOF
+        sudo useradd -m -d /var/www/html -s /bin/bash admin
+        echo "admin:Abcd1234!" | sudo chpasswd
+        sudo chown -R admin:admin /var/www/html
+        sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.bak
+
+        sudo bash -c 'cat > /etc/vsftpd.conf' <<EOF
 listen=YES
 listen_ipv6=NO
 anonymous_enable=NO
@@ -115,111 +120,121 @@ ssl_sslv3=NO
 rsa_cert_file=/etc/ssl/certs/vsftpd-selfsigned.crt
 rsa_private_key_file=/etc/ssl/private/vsftpd-selfsigned.key
 
-# Pasang userlist
+# Userlist settings
 userlist_enable=YES
 userlist_file=/etc/vsftpd.userlist
 userlist_deny=NO
 
-# Port 21
 listen_port=21
 EOF
-echo "admin" | sudo tee /etc/vsftpd.userlist
-sudo systemctl restart vsftpd
-sudo systemctl enable vsftpd
+
+        echo "admin" | sudo tee /etc/vsftpd.userlist
+        sudo systemctl restart vsftpd
+        sudo systemctl enable vsftpd
         ;;
     3)
-        echo "Pilihan anda: Install SSL Certificate HTTPS"
-read -p "Masukkan domain kamu (contoh: example.com): " domain
-email="admin@$domain"
+        clear
+        echo "Pilihan anda: Install SSL Certificate (HTTPS)"
+        read -p "Masukkan domain kamu (contoh: example.com): " domain
+        email="admin@$domain"
 
-# Cek apakah apache2 atau nginx terinstall
-if systemctl list-units --type=service | grep -q apache2; then
-    echo "Terdeteksi Apache terinstall."
-    sudo apt install -y python3-certbot-apache
-    sudo certbot --apache --non-interactive --agree-tos --redirect -m "$email" -d "$domain"
-    sudo systemctl restart apache2
-elif systemctl list-units --type=service | grep -q nginx; then
-    echo "Terdeteksi Nginx terinstall."
-    sudo apt install -y python3-certbot-nginx
-    sudo certbot --nginx --non-interactive --agree-tos --redirect -m "$email" -d "$domain"
-    sudo systemctl restart nginx
-else
-    echo "Anda belum menginstal Web Server (Apache atau Nginx)."
-fi
+        if systemctl list-units --type=service | grep -q apache2; then
+            echo "Terdeteksi Apache terinstall."
+            sudo apt install -y python3-certbot-apache
+            sudo certbot --apache --non-interactive --agree-tos --redirect -m "$email" -d "$domain"
+            sudo systemctl restart apache2
+        elif systemctl list-units --type=service | grep -q nginx; then
+            echo "Terdeteksi Nginx terinstall."
+            sudo apt install -y python3-certbot-nginx
+            sudo certbot --nginx --non-interactive --agree-tos --redirect -m "$email" -d "$domain"
+            sudo systemctl restart nginx
+        else
+            echo "Anda belum menginstal Web Server (Apache atau Nginx)."
+        fi
         ;;
     4)
-        read -p "Apakah kamu yakin akan mengunistall (y/n): " uninstall
+        clear
+        read -p "Apakah kamu yakin akan menguninstall semuanya? (y/n): " uninstall
         case $uninstall in
-        y)
-        #uninstal mysql
-if systemctl list-units --type=service | grep -q mysql; then
-sudo mysql <<EOF
+            y)
+                echo "Mulai proses uninstall..."
+
+                # Uninstall MySQL
+                if systemctl list-units --type=service | grep -q mysql; then
+                    sudo mysql <<EOF
 DROP USER IF EXISTS 'admin'@'localhost';
 FLUSH PRIVILEGES;
 EXIT
 EOF
-sudo chown -R root:root /var/www/html
-sudo chmod -R 755 /var/www/html
-sudo systemctl disable mysql
-sudo systemctl stop mysql
-sudo apt remove --purge -y php-curl php-mysql certbot python python3 mysql-server phpmyadmin
-fi
+                    sudo chown -R root:root /var/www/html
+                    sudo chmod -R 755 /var/www/html
+                    sudo systemctl disable mysql
+                    sudo systemctl stop mysql
+                    sudo apt remove --purge -y php-curl php-mysql certbot python python3 mysql-server phpmyadmin
+                fi
 
-#uninstall Apache/Nginx+certbot HTTPS
-if systemctl list-units --type=service | grep -q apache2; then
-sudo systemctl stop apache2
-sudo systemctl disable apache2
-sudo apt remove --purge -y python3-certbot-apache
-sudo apt remove --purge -y apache2 php libapache2-mod-php
-fi
+                # Uninstall Apache
+                if systemctl list-units --type=service | grep -q apache2; then
+                    sudo systemctl stop apache2
+                    sudo systemctl disable apache2
+                    sudo apt remove --purge -y python3-certbot-apache
+                    sudo apt remove --purge -y apache2 php libapache2-mod-php
+                fi
 
-if systemctl list-units --type=service | grep -q nginx; then
-sudo systemctl stop nginx
-sudo systemctl disable nginx
+                # Uninstall Nginx
+                if systemctl list-units --type=service | grep -q nginx; then
+                    sudo systemctl stop nginx
+                    sudo systemctl disable nginx
 
-if systemctl list-units --type=service | grep -q php7.4-fpm; then
-    echo "Menonaktifkan service PHP 7.4 FPM..."
-    sudo systemctl stop php7.4-fpm
-    sudo systemctl disable php7.4-fpm
-fi
+                    if systemctl list-units --type=service | grep -q php7.4-fpm; then
+                        sudo systemctl stop php7.4-fpm
+                        sudo systemctl disable php7.4-fpm
+                    fi
+                    if systemctl list-units --type=service | grep -q php8.1-fpm; then
+                        sudo systemctl stop php8.1-fpm
+                        sudo systemctl disable php8.1-fpm
+                    fi
 
-if systemctl list-units --type=service | grep -q php8.1-fpm; then
-    echo "Menonaktifkan service PHP 8.1 FPM..."
-    sudo systemctl stop php8.1-fpm
-    sudo systemctl disable php8.1-fpm
-fi
-sudo apt remove --purge -y python3-certbot-nginx
-sudo apt remove --purge -y nginx php-fpm
-fi
+                    sudo apt remove --purge -y python3-certbot-nginx
+                    sudo apt remove --purge -y nginx php-fpm
+                fi
 
-#uninstall FTP/FTPS
-if systemctl list-units --type=service | grep -q vsftpd; then
-sudo systemctl stop vsftpd
-sudo systemctl disable vsftpd
-sudo rm -f /etc/ssl/certs/vsftpd-selfsigned.crt
-sudo rm -f /etc/ssl/private/vsftpd-selfsigned.key
+                # Uninstall FTP/FTPS
+                if systemctl list-units --type=service | grep -q vsftpd; then
+                    sudo systemctl stop vsftpd
+                    sudo systemctl disable vsftpd
+                    sudo rm -f /etc/ssl/certs/vsftpd-selfsigned.crt
+                    sudo rm -f /etc/ssl/private/vsftpd-selfsigned.key
 
-if id "admin" &>/dev/null; then
-    echo "Menghapus user admin..."
-    sudo userdel -r admin
-fi
+                    if id "admin" &>/dev/null; then
+                        sudo userdel -r admin
+                    fi
 
-if [ -f /etc/vsftpd.conf.bak ]; then
-    sudo mv /etc/vsftpd.conf.bak /etc/vsftpd.conf
-fi
+                    if [ -f /etc/vsftpd.conf.bak ]; then
+                        sudo mv /etc/vsftpd.conf.bak /etc/vsftpd.conf
+                    fi
 
-sudo rm -f /etc/vsftpd.userlist
-sudo apt remove --purge -y vsftpd openssl
-fi
+                    sudo rm -f /etc/vsftpd.userlist
+                    sudo apt remove --purge -y vsftpd openssl
+                fi
 
-sudo apt autoremove -y
-sudo apt autoclean
-echo "Uninstall selesai!"
-        n) back to menu
+                # Bersihkan sistem
+                sudo apt autoremove -y
+                sudo apt autoclean
+                echo "Uninstall selesai!"
+                exit 0
+                ;;
+            n)
+                echo "Dibatalkan."
+                exit 0
+                ;;
+            *)
+                echo "Pilihan tidak tersedia!"
+                exit 1
+                ;;
+        esac
         ;;
     *)
         echo "Pilihan tidak tersedia!"
         ;;
 esac
-
-

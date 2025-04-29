2deefa5f1afcafdfdc81d69bdd884edfcf4edd0f
lamtota40 <lamtota40@gmail.com>
2025-04-29 14:50:48 +0700

    Update setup_web-server.sh

--- setup_web-server.sh
+++ setup_web-server.sh
@@ -1,214 +1,197 @@
 #!/bin/bash
 
+# Pastikan dijalankan sebagai root
 if [ "$(id -u)" -ne 0 ]; then
     echo "Harus dijalankan sebagai root!"
     exit 1
 fi
 
-sudo apt update -y
+# Update awal
+apt update -y
 
 while true; do
-clear
-echo "======================================"
-echo "        Menu Instalasi Server         "
-echo "======================================"
-echo ""
-echo "1. Install Web Server"
-echo "2. Certificate Web (HTTPS)"
-echo "3. Install FTP/FTPS"
-echo "4. Uninstall ALL"
-echo "0. Untuk KELUAR"
-echo ""
-echo "======================================"
-read -p "Masukan input anda: " pilihan
+    clear
+    echo "======================================"
+    echo "        Menu Instalasi Server         "
+    echo "======================================"
+    echo ""
+    echo "1. Install Web Server"
+    echo "2. Certificate Web (HTTPS)"
+    echo "3. Install FTP/FTPS"
+    echo "4. Uninstall ALL"
+    echo "0. Untuk KELUAR"
+    echo ""
+    echo "======================================"
+    read -p "Masukan input anda: " pilihan
 
-case $pilihan in
-    1)
-        clear
-        echo "======================================"
-        echo "        Pilih Web Server             "
-        echo "======================================"
-        echo "1. Apache (LAMP: Linux + Apache + MySQL + PHP)"
-        echo "2. Nginx (LEMP: Linux + Nginx + MySQL + PHP)"
-        echo "======================================"
-        read -p "Masukan input anda untuk memilih web server: " webserver
-        case $webserver in
-            1)
-                echo "Anda memilih Apache (LAMP)"
-                sudo apt install apache2 php libapache2-mod-php -y
-                sudo systemctl enable apache2
-                sudo systemctl restart apache2
-                ;;
-            2)
-                echo "Anda memilih Nginx (LEMP)"
-                sudo apt install nginx php-fpm -y
-                sudo systemctl enable nginx
-                sudo systemctl enable php7.4-fpm || sudo systemctl enable php8.1-fpm
-                sudo systemctl restart nginx
-                sudo systemctl restart php7.4-fpm || sudo systemctl restart php8.1-fpm
-                sudo rm -f /var/www/html/index.nginx-debian.html
-                ;;
-            *)
-                read -p "Tekan [Enter] untuk kembali ke menu utama..."
-                ;;
-                esac
-                if ! systemctl list-units --type=service | grep -q mysql; then
-                # Set password MySQL dan phpMyAdmin sebelum install
-        MYSQL_PASSWORD="Abcd1234!"
-        sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_PASSWORD"
-        sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_PASSWORD"
-        sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-        sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $MYSQL_PASSWORD"
-        sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $MYSQL_PASSWORD"
-        sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $MYSQL_PASSWORD"
-        sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+    case $pilihan in
+        1)
+            clear
+            echo "======================================"
+            echo "        Pilih Web Server             "
+            echo "======================================"
+            echo "1. Apache (LAMP: Linux + Apache + MySQL + PHP)"
+            echo "2. Nginx (LEMP: Linux + Nginx + MySQL + PHP)"
+            echo "======================================"
+            read -p "Masukan input anda untuk memilih web server: " webserver
 
-                # Lanjut install PHP module, MySQL, dan phpMyAdmin
-        sudo apt install php-curl php-mysql certbot python python3 -y
-        sudo apt install mysql-server phpmyadmin -y
-        sudo chown -R www-data:www-data /var/www/html
-        sudo chmod -R 755 /var/www/html
+            case $webserver in
+                1)
+                    echo "Anda memilih Apache (LAMP)"
+                    apt install -y apache2 php libapache2-mod-php
+                    systemctl enable apache2
+                    systemctl restart apache2
+                    ;;
+                2)
+                    echo "Anda memilih Nginx (LEMP)"
+                    apt install -y nginx php-fpm
+                    systemctl enable nginx
+                    systemctl enable php7.4-fpm || systemctl enable php8.1-fpm
+                    systemctl restart nginx
+                    systemctl restart php7.4-fpm || systemctl restart php8.1-fpm
+                    rm -f /var/www/html/index.nginx-debian.html
+                    ;;
+                *)
+                    read -p "Tekan [Enter] untuk kembali ke menu utama..."
+                    ;;
+            esac
 
-        # Setup user MySQL
-        sudo mysql <<EOF
+            if ! systemctl list-units --type=service | grep -q mysql; then
+                MYSQL_PASSWORD="Abcd1234!"
+                debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_PASSWORD"
+                debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_PASSWORD"
+                debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+                debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $MYSQL_PASSWORD"
+                debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $MYSQL_PASSWORD"
+                debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $MYSQL_PASSWORD"
+                debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+
+                apt install -y php-curl php-mysql certbot python python3
+                apt install -y mysql-server phpmyadmin
+                chown -R www-data:www-data /var/www/html
+                chmod -R 755 /var/www/html
+
+                mysql <<EOF
 CREATE USER IF NOT EXISTS 'web'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON *.* TO 'web'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-EXIT
 EOF
-        sudo systemctl enable mysql
-        sudo systemctl restart mysql
-fi
-        if ! id "web" &>/dev/null; then
-        sudo useradd -m -s /bin/bash web || true
-        echo "web:Abcd1234!" | sudo chpasswd
-sudo usermod -aG www-data web
-sudo chown -R web:web /var/www/html
-sudo usermod -d /var/www/html web
-        fi
-
-                read -p "Tekan [Enter] untuk kembali ke menu utama..."
-        ;;
-    2)
-        echo "Pilihan anda: Install SSL Certificate HTTPS"
-        echo "jika gagal setting DNS tambahkan CAA : 0 issue "letsencrypt.org""
-        read -p "Masukkan domain kamu (contoh: mydomain.com): " domain
-        email="admin@$domain"
-
-        # Cek apakah apache2 atau nginx terinstall
-        if systemctl list-units --type=service | grep -q apache2; then
-            echo "Terdeteksi Apache terinstall."
-            sudo apt install -y python3-certbot-apache
-            sudo certbot --apache --non-interactive --agree-tos --redirect -m "$email" -d "$domain"
-            sudo systemctl restart apache2
-        elif systemctl list-units --type=service | grep -q nginx; then
-            echo "Terdeteksi Nginx terinstall."
-            sudo apt install -y python3-certbot-nginx
-            sudo certbot --nginx --non-interactive --agree-tos --redirect -m "$email" -d "$domain"
-            sudo systemctl restart nginx
-        else
-            echo "Anda belum menginstal Web Server (Apache atau Nginx)."
-        fi
-        read -p "Tekan [Enter] untuk kembali ke menu utama..."
-        ;;
-            3)
-        echo "Pilihan anda: Install FTP/FTPS"
-        sudo apt install vsftpd -y
-sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.bak
-sudo sed -i 's/#\s*write_enable=YES/write_enable=YES/' /etc/vsftpd.conf
-sudo sed -i 's/#\s*chroot_local_user=YES/chroot_local_user=YES/' /etc/vsftpd.conf
-echo "allow_writeable_chroot=YES" | sudo tee -a /etc/vsftpd.conf
-sudo systemctl restart vsftpd
-sudo systemctl enable vsftpd
-read -p "Tekan [Enter] untuk kembali ke menu utama..."
-        ;;
-4)
-    read -p "Apakah kamu yakin akan meng-uninstall semua (y/n): " uninstall
-    case $uninstall in
-        y|Y)
-            echo "Mulai proses uninstall..."
-
-            # Uninstall MySQL dan PHPMyAdmin
-            if systemctl list-units --type=service | grep -q mysql; then
-                echo "Uninstalling MySQL dan PHPMyAdmin..."
-                sudo mysql <<EOF
-DROP USER IF EXISTS 'web'@'LEGES;
-EXIT
-EOF
-                sudo chown -R root:root /var/www/html
-                sudo chmod -R 755 /var/www/html
-                sudo systemctl disable mysql
-                sudo systemctl stop mysql
-                sudo DEBIAN_FRONTEND=noninteractive apt remove --purge -y php-curl php-mysql certbot python python3 mysql-server phpmyadmin
+                systemctl enable mysql
+                systemctl restart mysql
             fi
 
-            # Uninstall Apache
-            if systemctl list-units --type=service | grep -q apache2; then
-                echo "Uninstalling Apache..."
-                sudo systemctl stop apache2
-                sudo systemctl disable apache2
-                sudo apt remove --purge -y python3-certbot-apache
-                sudo apt remove --purge -y apache2 php libapache2-mod-php
+            if ! id "web" &>/dev/null; then
+                useradd -m -s /bin/bash web
+                echo "web:Abcd1234!" | chpasswd
+                usermod -aG www-data web
+                chown -R web:web /var/www/html
+                usermod -d /var/www/html web
             fi
 
-            # Uninstall Nginx + PHP-FPM
-            if systemctl list-units --type=service | grep -q nginx; then
-                echo "Uninstalling Nginx dan PHP-FPM..."
-                sudo systemctl stop nginx
-                sudo systemctl disable nginx
-
-                if systemctl list-units --type=service | grep -q php7.4-fpm; then
-                    sudo systemctl stop php7.4-fpm
-                    sudo systemctl disable php7.4-fpm
-                fi
-
-                if systemctl list-units --type=service | grep -q php8.1-fpm; then
-                    sudo systemctl stop php8.1-fpm
-                    sudo systemctl disable php8.1-fpm
-                fi
-
-                sudo apt remove --purge -y python3-certbot-nginx
-                sudo apt remove --purge -y nginx php-fpm
-            fi
-
-            # Uninstall FTP/FTPS (vsftpd)
-            if systemctl list-units --type=service | grep -q vsftpd; then
-                echo "Uninstalling FTP/FTPS..."
-                sudo systemctl stop vsftpd
-                sudo systemctl disable vsftpd
-
-                if [ -f /etc/vsftpd.conf.bak ]; then
-                    sudo mv /etc/vsftpd.conf.bak /etc/vsftpd.conf
-                fi
-                sudo apt remove --purge -y vsftpd
-            fi
-
-            # Cleanup
-            sudo apt autoremove -y
-            sudo apt autoclean
-            
-            #remove user admin
-                if id "web" &>/dev/null; then
-                    sudo userdel -r web
-                fi
-            echo "Uninstall selesai!..."
-            ;;
-        n|N)
-            echo "Batal uninstall..."
-            ;;
-        *)
-            echo "Pilihan tidak valid!"
             read -p "Tekan [Enter] untuk kembali ke menu utama..."
             ;;
-    esac
-    ;;
-            0)
+
+        2)
+            echo "Pilihan anda: Install SSL Certificate HTTPS"
+            echo "jika gagal setting DNS tambahkan CAA : 0 issue \"letsencrypt.org\""
+            read -p "Masukkan domain kamu (contoh: mydomain.com): " domain
+            email="admin@$domain"
+
+            if systemctl list-units --type=service | grep -q apache2; then
+                echo "Terdeteksi Apache terinstall."
+                apt install -y python3-certbot-apache
+                certbot --apache --non-interactive --agree-tos --redirect -m "$email" -d "$domain"
+                systemctl restart apache2
+            elif systemctl list-units --type=service | grep -q nginx; then
+                echo "Terdeteksi Nginx terinstall."
+                apt install -y python3-certbot-nginx
+                certbot --nginx --non-interactive --agree-tos --redirect -m "$email" -d "$domain"
+                systemctl restart nginx
+            else
+                echo "Anda belum menginstal Web Server (Apache atau Nginx)."
+            fi
+            read -p "Tekan [Enter] untuk kembali ke menu utama..."
+            ;;
+
+        3)
+            echo "Pilihan anda: Install FTP/FTPS"
+            apt install -y vsftpd
+            cp /etc/vsftpd.conf /etc/vsftpd.conf.bak
+            sed -i 's/#\s*write_enable=YES/write_enable=YES/' /etc/vsftpd.conf
+            sed -i 's/#\s*chroot_local_user=YES/chroot_local_user=YES/' /etc/vsftpd.conf
+            echo "allow_writeable_chroot=YES" | tee -a /etc/vsftpd.conf
+            systemctl restart vsftpd
+            systemctl enable vsftpd
+            read -p "Tekan [Enter] untuk kembali ke menu utama..."
+            ;;
+
+        4)
+            read -p "Apakah kamu yakin akan meng-uninstall semua (y/n): " uninstall
+            case $uninstall in
+                y|Y)
+                    echo "Mulai proses uninstall..."
+
+                    if systemctl list-units --type=service | grep -q mysql; then
+                        mysql <<EOF
+DROP USER IF EXISTS 'web'@'localhost';
+EOF
+                        chown -R root:root /var/www/html
+                        chmod -R 755 /var/www/html
+                        systemctl disable mysql
+                        systemctl stop mysql
+                        DEBIAN_FRONTEND=noninteractive apt remove --purge -y php-curl php-mysql certbot python python3 mysql-server phpmyadmin
+                    fi
+
+                    if systemctl list-units --type=service | grep -q apache2; then
+                        systemctl stop apache2
+                        systemctl disable apache2
+                        apt remove --purge -y python3-certbot-apache apache2 php libapache2-mod-php
+                    fi
+
+                    if systemctl list-units --type=service | grep -q nginx; then
+                        systemctl stop nginx
+                        systemctl disable nginx
+                        systemctl stop php7.4-fpm || true
+                        systemctl disable php7.4-fpm || true
+                        systemctl stop php8.1-fpm || true
+                        systemctl disable php8.1-fpm || true
+                        apt remove --purge -y python3-certbot-nginx nginx php-fpm
+                    fi
+
+                    if systemctl list-units --type=service | grep -q vsftpd; then
+                        systemctl stop vsftpd
+                        systemctl disable vsftpd
+                        [ -f /etc/vsftpd.conf.bak ] && mv /etc/vsftpd.conf.bak /etc/vsftpd.conf
+                        apt remove --purge -y vsftpd
+                    fi
+
+                    apt autoremove -y
+                    apt autoclean
+
+                    if id "web" &>/dev/null; then
+                        userdel -r web
+                    fi
+
+                    echo "Uninstall selesai!"
+                    ;;
+                n|N)
+                    echo "Batal uninstall..."
+                    ;;
+                *)
+                    echo "Pilihan tidak valid!"
+                    read -p "Tekan [Enter] untuk kembali ke menu utama..."
+                    ;;
+            esac
+            ;;
+
+        0)
             echo "Keluar dari program."
             exit 0
             ;;
-    *)
-        echo "Pilihan tidak tersedia!"
-        read -p "Tekan [Enter] untuk kembali ke menu utama..."
-        ;;
-esac
+
+        *)
+            echo "Pilihan tidak tersedia!"
+            read -p "Tekan [Enter] untuk kembali ke menu utama..."
+            ;;
+    esac
 done

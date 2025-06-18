ddd6c5cdfa86bf182c704ff8f3f7daa478b3a28b
lamtota40 <lamtota40@gmail.com>
2025-06-18 19:17:02 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install -y apache2 php php-curl libapache2-mod-php php-mysql mysql-server
 
-pass=Abcd1234!
+pass="Abcd1234!"
 echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/app-password-confirm password $pass" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/mysql/admin-pass password $pass" | debconf-set-selections
